b3d651ec31ffe09cca71e31c1fd8c89e7539593e
utshaav <utshaav583@gmain.com>
2022-04-05 20:59:53 +0545

    ML model created

---
+++ ClinicManagement/Controllers/MLController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClinicManagementML.Model;
+ 
+namespace ClinicManagement.Controllers
+{
+    public class MLController : Controller
+    {
+        // GET: ML
+        public ActionResult GetListOfDiseases()
+        {
+            ModelInput modelInput = new ModelInput;
+
+
+            foreach (var prop in modelInput.GetType().GetProperties())
+            {
+                
+            }
+            return View();
+        }
+    }
+}
